8388d61dfbd41dfecdeadd150144f67e8932a05c
christoferson <85178973+christoferson@users.noreply.github.com>
2021-06-22 21:02:02 +0900

    Update Company Service

--- src/main/java/org/demo/GreetingResource.java
+++ src/main/java/org/demo/GreetingResource.java
@@ -5,31 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/hello-resteasy")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello";
+        return "Hello RESTEasy";
     }
-
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public String helloHtml() {
-        return "<html><body><font style='color:blue;font-size:24;'>Hello HTML</font></body></html>";
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public String helloXml() {
-      return String.format("<?xml version=\"1.0\"?><hello>Hello XML</hello>");
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String helloJson() {
-      return String.format("{message:'Hello JSON'}");
-    }
-
 }
